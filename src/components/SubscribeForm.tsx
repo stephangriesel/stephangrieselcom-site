9630307fbd0fa25e3cfc721805f1cb212a813c16
Stephan Griesel <sgriesel@gmail.com>
2024-02-06 06:30:09 +0100

    submit handler

--- src/components/SubscribeForm.tsx
+++ src/components/SubscribeForm.tsx
@@ -1,13 +1,40 @@
+import { useState, useRef } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import validateEmail from "../lib/validateEmail";
+
 const SubscribeForm = () => {
+  const [isSumbitting, setIsSubmitting] = useState(false);
+  const formRef = useRef<HTMLFormElement>(null);
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("Submit!");
+    if(isSumbitting) return;
+    setIsSubmitting(true);
+
+    const subToast = toast.loading("Submitting");
+
+    const formData = new FormData(e.currentTarget);
+    const formInputs = Object.fromEntries(formData);
+    console.log("Handle Submits", formInputs)
+    
+    if(!formInputs?.email){
+      return toast.error("Please provide an email address", {
+        id: subToast,
+      })
+    }
+
+    if(!validateEmail((formInputs.email as string).trim())){
+      return toast.error("Please enter an valid email address", {
+        id: subToast,
+      })
+    }
   };
 
-  return (<form onSubmit={handleSubmit}>
+  return (<form ref={formRef} onSubmit={handleSubmit}>
     <label htmlFor="email">Enter your email</label>
-    <input type="email" name="email" id="email" />
-    <button type="submit">Subscribe</button>
+    <input type="email" name="email" id="email" required/>
+    <button type="submit" disabled={isSumbitting}>Subscribe</button>
+    <Toaster />
     </form>
   );
 };
