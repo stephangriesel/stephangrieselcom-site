e8a10c11ba2052d7b75924a177d6385990082e4a
Stephan Griesel <sgriesel@gmail.com>
2024-02-13 06:26:16 +0100

    more error checking

--- src/pages/api/subscribe.json.ts
+++ src/pages/api/subscribe.json.ts
@@ -1,6 +1,7 @@
 export const prerender = false;
 
 import type { APIRoute } from "astro"
+import validateEmail from "../../lib/validateEmail";
 
 export const POST: APIRoute = async ({ request }) => {
   try {
@@ -8,9 +9,16 @@ export const POST: APIRoute = async ({ request }) => {
     const { email } = body;
     console.log("POST: APIRoute", email);
 
-    if(email){
+    // Check if email exists
+    if(!email){
       throw new Error("Not working");
     }
+
+    // Validate email
+    if(!validateEmail(email as string)){
+      throw new Error("Not working");
+    }
+    
   } catch (error) {
     if (error instanceof Error) {
       console.log(error.message);
