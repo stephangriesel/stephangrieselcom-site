0a4229eba1ba97a7fef36070f7c2711025d75cb1
Stephan Griesel <sgriesel@gmail.com>
2024-02-13 06:14:39 +0100

    error handling test

--- src/pages/api/subscribe.json.ts
+++ src/pages/api/subscribe.json.ts
@@ -7,11 +7,21 @@ export const POST: APIRoute = async ({ request }) => {
     const body = await request.json();
     const { email } = body;
     console.log("POST: APIRoute", email);
+
+    if(email){
+      throw new Error("Not working");
+    }
   } catch (error) {
-    
+    if (error instanceof Error) {
+      console.log(error.message);
+      return new Response(null, {
+        status: 400,
+        statusText: error.message,
+      });
+    }
   }
-  return new Response(JSON.stringify({
-      message: "This was a POST!"
-    })
-  )
+  return new Response(null, {
+    status: 400,
+    statusText: "Unexpected Error!",
+  });
 }
